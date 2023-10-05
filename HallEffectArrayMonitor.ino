ccd99c4a0e31c5972a5a58bc88eba11d2756e81d
Matt Gaidica, Ph.D <matt@gaidi.ca>
2023-10-05 15:59:08 -0500

    Use an array and loop

--- HallEffectArrayMonitor.ino
+++ HallEffectArrayMonitor.ino
@@ -3,41 +3,29 @@
 #include "ADS1X15.h"
 
 ADS1115 ADS(0x48);
+ADS1115 ADS2(0x49);
+const int HALL_EN_GPIO = 13;
+float f, adcVals[8];
+int i;
 
-
-void setup() 
-{
+void setup() {
+  digitalWrite(HALL_EN_GPIO,1);
   Serial.begin(115200);
   ADS.begin();
-  Serial.println("\n\nHello, Hall.");
+  ADS.setGain(0);
+  f = ADS.toVoltage(1);  // voltage factor
+  Serial.println("\n\Initiated...");
 }
 
-void loop() 
-{
-  // Read input from the Serial Monitor
-  if (Serial.available()) {
-    char incomingChar = Serial.read();
-    
-    if (incomingChar == '\n') {
-      // Process the data after receiving a newline character
-      processReceivedData();
+void loop() {
+  for (i = 0;i < 8;i++) {
+    if (i < 4) {
+      Serial.print(ADS.readADC(i) * f);
+    } else {
+      Serial.print(ADS2.readADC(i-4) * f);
     }
-    if (incomingChar == 'r') {
-      Serial.println("--RESET\n\n");
-    }
+    Serial.print("\t");
   }
-}
-
-void processReceivedData() {
-  ADS.setGain(1);
-
-  int16_t val_0 = ADS.readADC(0);  
-  int16_t val_1 = ADS.readADC(1);  
-  int16_t val_2 = ADS.readADC(2);  
-
-  float f = ADS.toVoltage(1);  // voltage factor
-
-  Serial.print(val_0 * f, 3); Serial.print('\t');
-  Serial.print(val_1 * f, 3); Serial.print('\t');
-  Serial.print(val_2 * f, 3); Serial.println();
+  Serial.println("");
+  delay(500);
 }
