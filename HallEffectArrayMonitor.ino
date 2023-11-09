bf6ecc009f347b7cb2de8ba1cb7b796e2ea67232
Matt Gaidica, Ph.D <matt@gaidi.ca>
2023-11-09 14:44:31 -0600

    4-channel I2C

--- HallEffectArrayMonitor.ino
+++ HallEffectArrayMonitor.ino
@@ -1,15 +1,19 @@
 // by Matt Gaidica, PhD
 
 #include "ADS1X15.h"
+#define ADDR_GND 0x48
+#define ADDR_VDD 0x49
+#define ADDR_SDA 0x4A
+#define ADDR_SCL 0x4B
 
-ADS1115 ADS(0x48);
-ADS1115 ADS2(0x49);
+ADS1115 ADS(ADDR_SCL);
+ADS1115 ADS2(ADDR_SDA);
 const int HALL_EN_GPIO = 13;
 float f, adcVals[8];
 int i;
 
 void setup() {
-  digitalWrite(HALL_EN_GPIO,1);
+  digitalWrite(HALL_EN_GPIO, 1);
   Serial.begin(115200);
   ADS.begin();
   ADS2.begin();
@@ -20,14 +24,14 @@ void setup() {
 }
 
 void loop() {
-  for (i = 0;i < 8;i++) {
+  for (i = 0; i < 8; i++) {
     if (i < 4) {
       Serial.print(ADS.readADC(i) * f);
     } else {
-      Serial.print(ADS2.readADC(i-4) * f);
+      Serial.print(ADS2.readADC(i - 4) * f);
     }
     Serial.print("\t");
   }
   Serial.println("");
-  delay(500);
+  // delay(500);
 }
